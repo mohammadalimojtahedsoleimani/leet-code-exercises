cf25a08599d6a3b7e87e7b3803b84eedd8317e01
agha mamali <soleimanyali88@gmail.com>
2024-04-18 01:45:52 +0330

    roman to integer completed

--- Roman-to-Integer.py
+++ Roman-to-Integer.py
@@ -13,14 +13,24 @@ my_dict = {
     'CD': 400,
     'CM': 900
 }
+substrings_to_find = ["IV", "IX", "XL", "XC", 'CD', 'CM']
 
 
 def romanToInt(input_string):
+    skip_next = False
     total_value = 0
-    for i in range(0, len(input_string)):
-        if input_string[i] in my_dict.keys():
 
-        # return total_value
+    for i in range(len(input_string)):
+        if skip_next:
+            skip_next = False  # Reset the flag
+            continue
+        if input_string[i:i + 2] in my_dict.keys():
+            total_value += my_dict[input_string[i:i + 2]]
+            skip_next = True
+        else:
+            total_value += my_dict[input_string[i]]
+
+    return total_value
 
 
 print(romanToInt("MCMXCIV"))
