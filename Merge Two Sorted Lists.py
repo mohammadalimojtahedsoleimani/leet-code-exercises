6da4a4a721a4e06d79fdeaaa18b7ac9a728cdbe4
agha mamali <soleimanyali88@gmail.com>
2024-04-27 21:02:54 +0330

    Merge Two sorted linked list added

---
+++ Merge Two Sorted Lists.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
+
+
+def mergeTwoLists(list1, list2):
+    cur = dummy = Node()
+    while list1 and list2:
+        if list1.val < list2.val:
+            cur.next = list1
+            list1, cur = list1.next, list1
+        else:
+            cur.next = list2
+            list2, cur = list2.next, list2
+
+    if list1 or list2:
+        cur.next = list1 if list1 else list2
+
+    return dummy.next
+
+
+print(mergeTwoLists([1, 2, 4], [1, 3, 4]))
