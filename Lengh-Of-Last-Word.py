fc1010869daf9a02a3916e30c172f9b9a01ab760
agha mamali <soleimanyali88@gmail.com>
2024-05-08 00:03:48 +0330

    faster version of Lengh-Of-Last-Word.py solution added

--- Lengh-Of-Last-Word.py
+++ Lengh-Of-Last-Word.py
@@ -1,14 +1,6 @@
 def lengthOfLastWord(s):
-    temp = ''
-    flag = False
-    for i in range(len(s) - 1, -1, -1):
-        if s[i] != ' ':
-            temp += s[i]
-        elif s[-1] == ' ' and len(temp) == 0:
-            continue
-        else:
-            break
-    return len(temp)
+    temp = s.split()
+    return len(temp[-1])
 
 
 print(lengthOfLastWord("   fly me   to   the moon  "))
