a41fc20f66d5170ebd7f8e4af9fc1d932de98447
agha mamali <soleimanyali88@gmail.com>
2024-06-04 23:09:31 +0330

    Longest-Palindrome added

---
+++ Longest-Palindrome.py
@@ -0,0 +1,21 @@
+def longestPalindrome(s):
+    if len(s) == 1:
+        return 1
+    char_count = {}
+    for char in s:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    lengh = 0
+    odd_found = False
+    for count in char_count.values():
+        if count % 2 == 0:
+            lengh += count
+        else:
+            lengh += count - 1
+            odd_found = True
+    if odd_found:
+        lengh += 1
+    return lengh
