21d42457085c48a434456bd0658ca7046598df9b
agha mamali <soleimanyali88@gmail.com>
2024-06-07 18:08:40 +0330

    Replace-Words faster version added

--- Replace-Words.py
+++ Replace-Words.py
@@ -1,15 +1,19 @@
 def replaceWords(dictionary, sentence):
     sentence_new = sentence.split(' ')
+    temp = []
+    shortest_string = ''
     for word in range(len(sentence_new)):
-        for substring in dictionary:
-            if substring in sentence_new[word] and sentence_new[word].startswith(substring):
-                relevant_sub = [s for s in dictionary if sentence_new[word].startswith(s)]
-                print(relevant_sub)
-                shortest_string = min(relevant_sub, key=len)
+        temp = [s for s in dictionary if sentence_new[word].startswith(s)]
+        for d in temp:
+            if d in sentence_new[word]:
+                shortest_string = min(temp, key=len)
                 sentence_new[word] = sentence_new[word].replace(sentence_new[word], shortest_string)
+                print('hello')
                 break
+        temp.clear()
+
     result = " ".join(sentence_new)
     print(result)
 
 
-replaceWords(["catt","cat","bat","rat"], 'the cattle was rattled by the battery')
+replaceWords(["catt", "cat", "bat", "rat"], 'the cattle was rattled by the battery')
